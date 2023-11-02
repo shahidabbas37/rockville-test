32b0fe8538266b09eff80c41e6fadc1436dbc80f
Shahid Abbas <shahidabbas0315@gmail.com>
2023-11-02 21:36:13 +0500

    shahid abbas test

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const DB_URL = 'mongodb://root:DAue98FoN6tyN6vihJtQA2ECNCtXVVm4@localhost:27017/?authMechanism=DEFAULT&authSource=admin';
+export const DB_URL = 'mongodb+srv://rock-ville:fUwG84M8RVkWfoIc@shahid.coszq.mongodb.net/ShahidAbbas-Test?retryWrites=true&w=majority';
 export const JWT_SECRET_KEY = 'Akoi897654$@lhoslsbjiughhsLJIDSCBV';
 export const JWT_EXPIRY = '1d';
 export const PORT = '3000';
